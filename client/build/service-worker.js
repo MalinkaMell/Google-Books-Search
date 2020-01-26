6a241802dc13d6e0cbb490273ba8aefc5fe651a8
Maimell <malinka77@gmail.com>
2020-01-26 16:46:03 -0700

    works on local, lets check on heroku

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.723ef961f1dcab2504b2647dc0e0a247.js"
+  "/precache-manifest.7ea384568432a840f4b37de486202679.js"
 );
 
 self.addEventListener('message', (event) => {
